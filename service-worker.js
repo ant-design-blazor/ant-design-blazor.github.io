fc8ecdb6ea1d9dbca248b7cb4e3fdf5c17d8e381
ElderJames <ElderJames@users.noreply.github.com>
2020-12-14 08:44:53 +0000

    deploy: ant-design-blazor/ant-design-blazor@21ddb9e9e0ec32bd72ff49987dc7e1da1569bb5c

--- service-worker.js
+++ service-worker.js
@@ -46,5 +46,5 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
-/* updated 2020-12-14 05:08:24 */
+/* updated 2020-12-14 08:41:51 */
 /* Manifest version: KjEnHSWB */
